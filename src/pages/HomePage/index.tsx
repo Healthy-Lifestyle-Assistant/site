6b3ba08333ab2bc8311e7e0b7f0ebc06db565e76
Vladyslav Penkin <vy891078@gmail.com>
2024-07-20 15:57:35 +0300

    improved website setup

--- src/pages/HomePage/index.tsx
+++
@@ -1,6 +0,0 @@
-import { FC } from 'react';
-import './styles.scss';
-
-export const HomePage: FC = () => {
-  return (<></>);
-};
--- src/pages/NotFoundPage/index.tsx
+++
@@ -1,6 +0,0 @@
-import { FC } from 'react';
-import './styles.scss';
-
-export const NotFoundPage: FC = () => {
-  return (<></>);
-};
