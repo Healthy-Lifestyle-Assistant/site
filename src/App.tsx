db34a520dd396382dc09377b62f026f0b0145ad1
Vladyslav Penkin <vy891078@gmail.com>
2024-08-19 21:55:28 +0300

    [NOV-25] Add logo

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import './App.scss';
 import './i18n/i18n.config';
 import { Navigate, Outlet, Route, Routes } from 'react-router-dom';
 import { HomePage } from '@pages/HomePage';
-import { BlogPage } from '@pages/BlogPage';
-import { ArticlePage } from '@pages/ArticlePage';
+// import { BlogPage } from '@pages/BlogPage';
+// import { ArticlePage } from '@pages/ArticlePage';
 import { Header } from '@components/Header';
 import { Footer } from '@components/Footer';
 
@@ -21,10 +21,10 @@ export const App: FC = () => {
               <Route path="services" element={<HomePage />} />
               <Route path="team" element={<HomePage />} />
               <Route path="contact-us" element={<HomePage />} />
-              <Route path="blog" element={<Outlet />}>
+              {/* <Route path="blog" element={<Outlet />}>
                 <Route index element={<BlogPage />} />
                 <Route path=":articleId" element={<ArticlePage />} />
-              </Route>
+              </Route> */}
               <Route path="*" element={<Navigate to="/" />} />
             </Route>
           </Routes>
