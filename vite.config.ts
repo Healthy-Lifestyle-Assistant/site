caef9162543308d1488b52c9bf5b12209bb7b9b0
Vladyslav Penkin <vy891078@gmail.com>
2024-08-20 14:26:38 +0300

    [NOV-23] Research hosting options: fixed base

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     },
   },
   plugins: [react()],
-  base: "/novarserve-website/",
+  base: "/",
   build: {
     rollupOptions: {
       external: ['intl-pluralrules']
