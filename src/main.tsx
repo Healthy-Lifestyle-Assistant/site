381183243ada08c2029854828a39e56a9855afa4
Vladyslav Penkin <vy891078@gmail.com>
2024-08-20 16:57:51 +0300

    [NOV-23] Fix SPA problems

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { App } from './App.tsx'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Router>
+  <Router basename="/">
     <React.StrictMode>
       <App />
     </React.StrictMode>
